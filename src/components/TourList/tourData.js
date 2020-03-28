6393df5d105bad31d47d3467af5366da6f2a129f
Sourav-GH <sourav.k.2k9@gmail.com>
2020-03-28 23:27:29 +0530

    first commit

---
+++ src/components/TourList/tourData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Agra",
+    img: "./img/tajmahal.jpg",
+    name: "the taj mahal tour",
+    info:
+      "The Taj Mahal is made in 16th centuary by Mugal Emperor Shah Jahan for his wife Mumtaz monument.!"
+  },
+  {
+    id: 2,
+    city: "New Delhi",
+    img: "./img/akshardham.jpg",
+    name: "akshardham temple tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 3,
+    city: "New Delhi",
+    img: "./img/indiagate.jpg",
+    name: "india gate tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 4,
+    city: "Kolkata",
+    img: "./img/victoria.jpg",
+    name: "victoria  tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  }
+];
